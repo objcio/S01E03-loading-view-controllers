2c3525d4bdfafb1f9143248552c9428aa499b3d9
Florian Kugler <mail@floriankugler.com>
2016-07-17 09:22:44 +0200

    Remove invalid playground page

--- Loading View Controllers.playground/Pages/Starting Point.xcplaygroundpage/Contents.swift
+++ Loading View Controllers.playground/Pages/Starting Point.xcplaygroundpage/Contents.swift
@@ -1,10 +1,10 @@
-import UIKit
+//: To run this playground start a SimpleHTTPServer on the commandline like this:
+//:
+//: `python -m SimpleHTTPServer 8000`
+//:
+//: It will serve up the current directory, so make sure to be in the directory containing episode.json
 
-// To run this playground start a SimpleHTTPServer on the commandline like this:
-//
-// python -m SimpleHTTPServer 8000
-//
-// It will serve up the current directory, so make sure to be in the directory containing episode.json
+import UIKit
 
 let url = NSURL(string: "http://localhost:8000/episode.json")!
 let episodeResource = Resource<Episode>(url: url, parseJSON: { anyObject in
